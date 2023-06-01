cc38ebd98cb18e892a14ee4d7603ac3107b01d4f
ofirroth <ofirroz5277@gmail.com>
2023-06-01 22:46:40 +0300

    commit getContact

--- Server/models/chatCounterSchema.js
+++ Server/models/chatCounterSchema.js
@@ -1,3 +1,7 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
 const chatCounterSchema = new Schema({
     count: {
       type: Number,
--- Server/services/chat.js
+++ Server/services/chat.js
@@ -1,8 +1,9 @@
-const Chat = require('../models/chat');
-const User = require('../models/user');
 const chatService = require('../services/chat')
 const userService = require('../services/user')
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const Chat = require('../models/chat');
+const ChatCounter = require('../models/chatCounterSchema');
 
 const createChat = async (loggedInUser,username1) => {
     const loggedUser = await User.findOne({ username: loggedInUser });
@@ -10,20 +11,21 @@ const createChat = async (loggedInUser,username1) => {
         //const last = { id: null, created: null, content: null };
         const user = await User.findOne({ username: username1 });
         if (user) {
-            // const chatCounter = await ChatCounter.findOne();
-            // let chatId = 0;
-            // if (chatCounter) {
-            //     // Increment the chat ID counter and save the updated value
-            //     chatCounter.count += 1;
-            //     await chatCounter.save();
-            //     chatId = chatCounter.count;
-            // } else {
-            //     // Create the chat ID counter document with an initial value of 1
-            //     const newChatCounter = new ChatCounter();
-            //     await newChatCounter.save();
-            // }
+            const chatCounter = await ChatCounter.findOne();
+            let chatId = 0;
+            if (chatCounter) {
+                // Increment the chat ID counter and save the updated value
+                chatCounter.count += 1;
+                await chatCounter.save();
+                chatId = chatCounter.count;
+            } else {
+                // Create the chat ID counter document with an initial value of 1
+                const newChatCounter = new ChatCounter();
+                await newChatCounter.save();
+            }
             const last = { id: null, created: null, content: null };
-            const chat = new Chat({ id: 0, users: [loggedUser, user], lastMessage: last });
+            const chat = new Chat({ id: chatId, users: [loggedUser, user], lastMessage: last });
+            await chat.save();
             const x = {id: chat.id, user};
             return x
         }
@@ -33,8 +35,6 @@ const createChat = async (loggedInUser,username1) => {
 };
 
 
-
-
 const getChats = async (token) => {
     try {
         const data = jwt.verify(token, "Some super secret key shhhhhhhhhhhhhhhhh!!!!!");
@@ -42,13 +42,38 @@ const getChats = async (token) => {
             return [];
         }
         else {
-            const chats = await Chat.find({ 'user.username': data.username });
-            //return chats;
+            const chats = await Chat.find({'users.username': data.username });
+            const contactList = chats.map((chat) => {
+                const otherUsers = chat.users.filter((user) => user.username !==  data.username );
+                var size= chat.messages.length
+                var lastMessage;
+                if(size==0)
+                {
+                lastMessage= {
+                    created: null
+                }
+                }
+                else
+                {
+                    lastMessage={
+                        created: chat.messages[size-1].created
+                    }
+                }
+                // Access the chat properties and perform operations
+                const contact = {
+                  id: chat.id,
+                  user: otherUsers[0], //we have just one user in users that have other username
+                  lastMessage: chat.lastMessage,
+                  // Perform additional modifications as needed
+                };
+                return contact;
+            });
+            return contactList;
         }
     } catch (error) {
         // Handle error if the query fails
         console.error('Error occurred:', error);
-        return [];
+        return[];
     }
 }
 
