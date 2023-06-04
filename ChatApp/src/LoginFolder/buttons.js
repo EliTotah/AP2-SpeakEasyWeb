3825dac67dd3635422a4c79d7d5b915a561ef1ae
EliTotah <totah5034020@gmail.com>
2023-06-04 23:48:04 +0300

    update 23:50

--- ChatApp/src/LoginFolder/buttons.js
+++ ChatApp/src/LoginFolder/buttons.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import './/buttons.css'
 import { useNavigate } from 'react-router-dom';
 
+//<Link to="/forget" id="forgetpass" className='link'>Forget Password?</Link>
 
 function Buttons() {
 
@@ -12,7 +13,6 @@ function Buttons() {
                 <span></span>
                 <small className="rmb">Remember me</small>
             </label>
-            <Link to="/forget" id="forgetpass" className='link'>Forget Password?</Link>
             <input type="submit" value="Sign in" className="btn3"/>
             <Link to="/SignUp" id="dnthave" className='link'>Don't have an account? Sign up</Link>
         </div>
